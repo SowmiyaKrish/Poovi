0a3db8354dca581fd4f060c916c9ca3b8524b2b6
SowmiyaKrish <sowmiyasmart218@gmail.com>
2017-06-07 17:49:39 +0530

    Create Posnegzero.java

---
+++ Posnegzero.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+public class Posnegzero
+{
+public static void main(string args[])
+{
+Scanner can=new Scanner(System.in);
+int num=can.nextInt();
+if(num==0)
+{
+System.out.println("zero");
+}
+else
+{
+if(num>0)
+{
+System.out.println("positive");
+}
+else
+{
+System.out.println("Negative");
+}
+}
+}
+}
